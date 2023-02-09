30c997fb4c42d22760d95d6b1e04711fe9fa7f6b
Phu-Pwint <85929590+Phu-Pwint@users.noreply.github.com>
2023-02-09 13:18:57 +0900

    Add files via upload

---
+++ pythonlec09ex1&2.py
@@ -0,0 +1,113 @@
+# lesson 1
+def printLine(n):
+    print("==== 課題",n,"====")
+
+
+
+# lesson 2
+# printLine(2)
+# def plus3(a,b,c):
+#     result = a+b+c
+#     return result
+
+# print(plus3(1, 2, 3))
+
+# lesson 3
+# printLine(3)
+# def mini(a,b):
+#     if a < b:
+#         return a
+#     elif a == b:
+#         return a
+# n1 = int(input("Firstnumber:"))
+# n2 = int(input(("Second number:")))
+# minimum = mini(n1,n2)
+# print("The minimum number is: ",minimum)
+
+# Lesson4
+# printLine(4)
+# def maxi(a,b):
+#     if b < a:
+#         return a
+#     elif a == b:
+#         return a
+# n1 = int(input("Firstnumber:"))
+# n2 = int(input(("Second number:")))
+# maximum = maxi(n1,n2)
+# print("The maximum number is: ",maximum)
+
+# Lesson 5
+# printLine(5)
+# def sumList():
+#     total = 0
+#     list1 = [1,2,3]
+#     total = sum(list1)
+#     return total
+# print(sumList())
+
+# def sumList(l):
+#     s = 0
+#     for n in l:
+#         s += n
+#     return s
+# print(sumList([11,22,33]))
+
+# Lesson 6
+# printLine(6)
+# def minInList(lst):
+#     return min(lst)
+
+# list1 = list(input())
+# print(minInList(list1))
+
+# Lesson 7
+# printLine(7)
+# def maxInList(lst):
+#     return max(lst)
+
+# list1 = list(input())
+# print(maxInList(list1))
+
+
+import random
+
+# Lesson 8
+# def inputDigit():
+#     while True:
+#         sent = input("数値を入力してください:")
+#         if sent.isdigit():
+#             return int(sent)
+
+# print(inputDigit())
+
+# Lesson 9
+# def intToString(num):
+#     if num>=0 and num<=9:
+#         arr = ["zero","one","two","three","four","five"]
+#         return arr[num]
+#     else:
+#         return None
+
+
+# num = int(input("Enter your number: "))
+
+# print(intToString(num))
+
+# lesson 10
+
+# lesson 11
+# def reverseList():
+#     fruits = ["apple","orange","banana"]
+#     return fruits[::-1]
+
+# # lst = list(input()).reverse()
+# print(reverseList())
+
+
+#Lesson 12
+def inputStrings():
+    sen = input("Type here: ")
+
+
+
+inputStrings()
